ed8bedb50c05818a3afa41eb2afdf3b8c0e5e0b1
Aiting Zhou <zhou0248@algonquinlive.com>
2024-12-11 16:43:47 -0500

    save movie selection screen

---
+++ lib/screens/selectionScreen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class MovieSelectionPage extends StatefulWidget {
+  const MovieSelectionPage({super.key});
+
+  @override
+  State<MovieSelectionPage> createState() => _MovieSelectionState();
+}
+
+class _MovieSelectionState extends State<MovieSelectionPage> {
+  final baseUrl = 'https://api.themoviedb.org/3/movie/popular';
+  final imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
+
+  List movies = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchMovies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Movie Choice'),
+      ),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Center(
+              child: Dismissible(
+                key: Key(movies[0]['id'].toString()),
+                onDismissed: (direction) {
+                  setState(() {
+                    movies.removeAt(0);
+                  });
+                },
+                child: Stack(
+                  children: [
+                    Image.network('$imageBaseUrl${movies[0]['poster_path']}'),
+                  ],
+                ),
+              ),
+            ),
+    );
+  }
+
+  Future<void> _fetchMovies() async {
+    final apiKey = dotenv.env['TMDB_API_KEY'];
+    final response = await http.get(Uri.parse('$baseUrl?api_key=$apiKey'));
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      setState() {
+        movies = data['results'];
+        isLoading = false;
+      }
+    } else {
+      setState(() {
+        isLoading = true;
+      });
+    }
+  }
+}
